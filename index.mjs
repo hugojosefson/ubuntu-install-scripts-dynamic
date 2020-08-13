de7efe936217c283e32be03fd37ac0985d3c91d3
Hugo Josefson <hugo@josefson.org>
2020-08-13 13:22:27 +0000

    Arrow functions.

--- index.mjs
+++ index.mjs
@@ -5,15 +5,13 @@ const argv = minimist(process.argv.slice(2))
 const names = argv._
 if (names.length) {
   install(names).then(
-    function (results) {
+    results => {
       if (argv.verbose) {
         console.dir(results)
       }
       console.log('Yay! Installed', names)
     },
-    function (result) {
-      console.error('Failed to install', result)
-    }
+    result => console.error('Failed to install', result)
   )
 } else {
   console.log('Usage: ' + process.argv[1] + ' <package-or-script>...')
