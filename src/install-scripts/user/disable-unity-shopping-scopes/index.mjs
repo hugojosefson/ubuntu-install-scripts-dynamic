53d7696fadfc76645a9013a1ef9eaed8aa475723
Hugo Josefson <hugo@josefson.org>
2020-08-14 12:10:39 +0200

    Revert "Extra-log user/disable-unity-shopping-scopes."
    This reverts commit 2878991174ce2bd0fcb8933ccfcc1bda869e8461.

--- src/install-scripts/user/disable-unity-shopping-scopes/index.mjs
+++ src/install-scripts/user/disable-unity-shopping-scopes/index.mjs
@@ -8,13 +8,10 @@ import scopes from './scopes.mjs'
 const SCRIPT_NAME = scriptName(import.meta.url)
 const inPathSync = inPath.sync
 
-export default async () => {
-  console.log(`${SCRIPT_NAME}: Start.`)
+export default () => {
   if (inPathSync('gsettings')) {
-    console.log(`${SCRIPT_NAME}: Running gsettings.`, scopes)
     return exec(command(scopes)).then(() => SCRIPT_NAME)
   } else {
-    console.log(`${SCRIPT_NAME}: Never mind, we don't have gsettings.`)
     return Promise.resolve(SCRIPT_NAME)
   }
 }
